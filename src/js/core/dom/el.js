c78cbf6235686ba98db722bb61e91bed2f6c8c2f
Anton Bobrov <anton.bobrov@hotmail.com>
2019-12-05 21:29:37 +0300

    Children - a new feature

--- src/js/core/dom/el.js
+++ src/js/core/dom/el.js
@@ -103,6 +103,8 @@ export function removeChildren (parent) {
  * @param {string|HTMLElement|Window} selector - Either a string selector or an element.
  * @param {HTMLElement|boolean} [outer] - An outer element. It will work only for string selectors.
  * 
+ * @returns {HTMLElement} - Returns anelement.
+ * 
  * @memberof Vevet.utils
  * @alias Vevet.utils.element
  */
@@ -133,6 +135,8 @@ export function element(selector, outer = false) {
  * @param {string|HTMLElement|NodeList|Array<HTMLElement>|Window} selector - A string selector, an element or a node list.
  * @param {HTMLElement|boolean} [outer] - An outer element. It will work only for string selectors.
  * 
+ * @returns {NodeList} - Returns elements.
+ * 
  * @memberof Vevet.utils
  * @alias Vevet.utils.element
  */
@@ -158,5 +162,46 @@ export function elements(selector, outer = false) {
             return document.querySelectorAll(selector);
         }
     }
+
+}
+
+
+
+/**
+ * @description Get child elements that match a certain selector.
+ * 
+ * @param {string} selector - A string selector.
+ * @param {HTMLElement} outer - An outer element.
+ * @param {number} [level=1] - What level of nesting.
+ * 
+ * @returns {Array<HTMLElement>} - Returns all children that match the selector.
+ * 
+ * @memberof Vevet.utils
+ * @alias Vevet.utils.element
+ */
+export function children(selector, outer = false, level = 1) {
+
+    let elements = [],
+        els = outer.children;
+
+    if (els.length === 0) {
+        return [];
+    }
+
+    if (level <= 1) {
+        for (let i = 0; i < els.length; i++) {
+            let el = els[i];
+            if (el.matches(selector)) {
+                elements.push(el);
+            }
+        }
+    }
+    else {
+        for (let i = 0; i < els.length; i++) {
+            elements = elements.concat(children(selector, els[i], level - 1));
+        }
+    }
+
+    return elements;
 
 }
