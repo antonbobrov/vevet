f36bd5b7710836d60ebe2e44cf12468f31f5ebf4
Anton Bobrov <anton.bobrov@hotmail.com>
2020-02-20 14:23:41 +0300

    id export change

--- src/js/id.js
+++ src/js/id.js
@@ -15,10 +15,12 @@
  * // => 'name_1563641265586_284'
  */
 
-export function id (prefix = 'id') {
+function id (prefix = 'id') {
 
     let id = `${+new Date()}_${Math.round(Math.random() * 1000)}`;
 
     return `${prefix}_${id}`;
 
 }
+
+export default id;
