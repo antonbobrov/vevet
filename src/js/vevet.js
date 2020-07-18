ff2c48547ec3c457c5105d64fa4dd5cb20dd6ab4
Anton Bobrov <anton.bobrov@hotmail.com>
2020-07-18 03:27:09 +0300

    :fire: Delete JS

--- src/js/vevet.js
+++
@@ -1,9 +0,0 @@
-/**
- * Vevet - a JavaScript library with useful stuff.
- * 
- * @module Vevet
- * @interface
- * @namespace
- * @author Anton Bobrov {@link https://github.com/antonbobrov/vevet.git| GitHub}
- * 
- */
