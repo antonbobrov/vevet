5507aaaf8ed293564b6ad82295f1ee670b777925
Anton Bobrov <anton.bobrov@hotmail.com>
2020-07-18 04:20:02 +0300

    Add Viewport to index

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,11 +1,13 @@
 import { Application } from "./base/Application"
 import { Callbacks } from "./base/Callbacks"
 import { Load } from "./base/Load"
+import { Viewport } from "./base/Viewport"
 
 export {
 
     Application,
     Callbacks,
-    Load
+    Load,
+    Viewport
 
 }
