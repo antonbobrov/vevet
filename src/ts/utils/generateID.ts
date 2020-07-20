1bbab1372f5df71228dbe3893ccb120efa5bebe2
Anton Bobrov <anton.bobrov@hotmail.com>
2020-07-21 01:09:03 +0300

    Update generateID

--- src/ts/utils/generateID.ts
+++ src/ts/utils/generateID.ts
@@ -1,9 +1,7 @@
-function generateID(prefix = 'id') {
-
-    let id = `${+new Date()}_${Math.round(Math.random() * 1000)}`;
-
+/**
+ * Generate an ID
+ */
+export default function generateID (prefix = "id") {
+    const id = `${+new Date()}_${Math.round(Math.random() * 1000)}`;
     return `${prefix}_${id}`;
-
 }
-
-export default generateID;
