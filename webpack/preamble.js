d607b622ec98f93c67e20a2504382948848b01c2
Anton Bobrov <anton.bobrov@hotmail.com>
2020-11-06 02:59:52 +0300

    Update module base

---
+++ webpack/preamble.js
@@ -0,0 +1,10 @@
+module.exports = `/*
+────────────────╔╗─
+╔═╦═╗╔═╗╔═╦═╗╔═╗║╚╗
+╚╗║╔╝║╩╣╚╗║╔╝║╩╣║╔╣
+─╚═╝─╚═╝─╚═╝─╚═╝╚═╝
+
+GitHub Repository: https://github.com/antonbobrov/vevet
+
+Powered by Anton Bobrov | https://github.com/antonbobrov
+*/`;
