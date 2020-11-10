f422a97b56169b9c38d4818ef6686f631b876625
Anton Bobrov <anton.bobrov@hotmail.com>
2020-11-11 00:39:34 +0300

    remove files

--- src/ts/utils/math/math.ts
+++
@@ -1,9 +0,0 @@
-import scopeProgress from "./scopeProgress";
-import boundProgress from "./boundProgress";
-import lerp from "./lerp";
-
-export const math = {
-    scopeProgress,
-    boundProgress,
-    lerp,
-};
--- src/ts/utils/utils.ts
+++
@@ -1,9 +0,0 @@
-import generateID from "./common/generateID";
-import mergeWithoutArrays from "./common/mergeWithoutArrays";
-import timeoutCallback from "./common/timeoutCallback";
-
-export const utils = {
-    generateID,
-    mergeWithoutArrays,
-    timeoutCallback,
-};
