91185cc5f3cb4976a3fa901d49c0d5b66883563f
Anthony Bobrov <anton.bobrov@hotmail.com>
2021-07-09 10:23:57 +0300

    Add common utils

---
+++ src/ts/utils/common/index.ts
@@ -0,0 +1,9 @@
+import randID from './randID';
+import mergeWithoutArrays from './mergeWithoutArrays';
+import timeoutCallback from './timeoutCallback';
+
+export {
+    randID,
+    mergeWithoutArrays,
+    timeoutCallback,
+};
