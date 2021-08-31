6d481b4e26cf14ee1954f0e2a4a0b40cc057436b
Anthony Bobrov <anton.bobrov@hotmail.com>
2021-08-31 17:39:25 +0300

    Add Timeline component examples

---
+++ examples-src/pages/components/timeline/sample.ts
@@ -0,0 +1,39 @@
+import { Easing } from 'easing-progress';
+import * as Vevet from '../../../../src/ts/index';
+
+new Vevet.Application();
+
+// get visualizer
+const visualizerThumb = document.querySelector<HTMLElement>('#timeline-visualizer-thumb');
+
+// create a timeline
+const timeline = new Vevet.Timeline({
+    duration: 5000,
+    easing: Easing.easeOutBounce,
+});
+timeline.addCallback('progress', (data) => {
+    if (visualizerThumb) {
+        visualizerThumb.style.left = `${data.easing * 100}%`;
+    }
+});
+timeline.play();
+
+// controls
+const playButton = document.querySelector('#timeline-play');
+if (playButton) {
+    playButton.addEventListener('click', () => {
+        timeline.play();
+    });
+}
+const pauseButton = document.querySelector('#timeline-pause');
+if (pauseButton) {
+    pauseButton.addEventListener('click', () => {
+        timeline.pause();
+    });
+}
+const reverseButton = document.querySelector('#timeline-reverse');
+if (reverseButton) {
+    reverseButton.addEventListener('click', () => {
+        timeline.reverse();
+    });
+}
--- examples-src/ts/index.ts
+++ examples-src/ts/index.ts
@@ -35,6 +35,9 @@ switch (pageNameAttr) {
     case 'components-scroll-view':
         import('../pages/components/scroll/scroll-view/sample');
         break;
+    case 'components-timeline':
+        import('../pages/components/timeline/sample');
+        break;
     case 'components-static-timeline':
         import('../pages/components/timeline/static-timeline/sample');
         break;
