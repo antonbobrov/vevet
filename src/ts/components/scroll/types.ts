53c97f42e544d2a5e619b0e1c564918c74caab28
Anthony Bobrov <anton.bobrov@hotmail.com>
2021-09-04 17:55:30 +0300

    Add scrollTo method

--- src/ts/components/scroll/types.ts
+++ src/ts/components/scroll/types.ts
@@ -1,5 +1,7 @@
 export interface ScrollableElement {
     scrollTop: number;
+    scrollTo(options: ScrollToOptions): void;
+    scrollTo(x: number, y: number): void;
     scrollLeft: number;
     scrollWidth: number;
     scrollHeight: number;
