74f3498638c0a3fc4cb655b001a0825f9e5c8c01
Anthony Bobrov <anton.bobrov@hotmail.com>
2022-03-29 13:18:56 +0300

    Add scrollToElement

--- src/ts/utils/scroll/index.ts
+++ src/ts/utils/scroll/index.ts
@@ -1,7 +1,9 @@
 import getValues from './getScrollValues';
 import scrollTo from './scrollTo';
+import scrollToElement from './scrollToElement';
 
 export {
     getValues,
     scrollTo as to,
+    scrollToElement as toElement,
 };
