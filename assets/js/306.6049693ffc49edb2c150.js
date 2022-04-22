603c186971e214383620ffce73c6e2403029b8cd
Anthony Bobrov <anton.bobrov@hotmail.com>
2022-04-22 16:10:30 +0300

    Update github pages

---
+++ assets/js/306.6049693ffc49edb2c150.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkvevet=self.webpackChunkvevet||[]).push([[306],{306:(e,r,a)=>{a.r(r);var n=a(832);new n.Application;var o=document.querySelector("#v-preloader");if(o){var c=o.querySelector("span");new n.ProgressPreloader({container:"#v-preloader"}).addCallback("progress",(function(e){c.innerHTML="".concat((100*e.progress).toFixed(0),"%")}))}}}]);
