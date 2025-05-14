78a8799bd0c43a295a937b852a3d28cda545763f
Anthony Bobrov <anton.bobrov@hotmail.com>
2025-05-14 15:22:02 +0400

    SplitText: restore node styles on destroy

--- src/components/SplitText/utils/saveInitialNodes.ts
+++ src/components/SplitText/utils/saveInitialNodes.ts
@@ -1,5 +1,6 @@
 interface IClone {
   node: Node;
+  cssText: string | null;
   parent?: Node | null;
 }
 
@@ -8,7 +9,11 @@ export function saveInitialNodes(root: Node) {
 
   function copy(node: Node): void {
     // Add the current node and its parent to the array
-    flatArray.push({ node, parent: node.parentNode });
+    flatArray.push({
+      node,
+      cssText: node instanceof HTMLElement ? node.style.cssText : null,
+      parent: node.parentNode,
+    });
 
     // Recursively process child nodes
     node.childNodes.forEach((child) => copy(child));
@@ -23,8 +28,14 @@ export function saveInitialNodes(root: Node) {
       const fragment = document.createDocumentFragment();
 
       flatArray.forEach((element) => {
-        while (element.node.childNodes[0]) {
-          element.node.childNodes[0].remove();
+        const { node, cssText } = element;
+
+        if (node instanceof HTMLElement && cssText) {
+          node.style.cssText = cssText;
+        }
+
+        while (node.childNodes[0]) {
+          node.childNodes[0].remove();
         }
 
         if (element.parent) {
