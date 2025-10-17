9b7eed3e3a67ce6afd272c352e7b704bf9e57138
Anton Bobrov <anton.bobrov@hotmail.com>
2025-10-17 12:55:19 +0300

    closest: return target if values are empty

--- src/utils/common/closest.ts
+++ src/utils/common/closest.ts
@@ -5,6 +5,10 @@
  * closest(5, [3, 6, 9, 12, 15]); // => 6
  */
 export function closest(target: number, values: number[]) {
+  if (!Array.isArray(values) || values.length === 0) {
+    return target;
+  }
+
   return values.reduce((prev, curr) =>
     Math.abs(curr - target) < Math.abs(prev - target) ? curr : prev,
   );
